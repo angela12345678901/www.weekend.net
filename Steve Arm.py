bffced5ec420d4f685beafef72e6c5bf7d918e87
angela12345678901 <manyicraft@gmail.com>
2026-06-16 18:13:51 -0500

    Update Steve Arm.py

--- Steve Arm.py
+++ Steve Arm.py
@@ -2,4 +2,4 @@ masa_total = 44444240 # kg
 c = 299792458 # m/s
 energia = masa_total * (c ** 2)
 print(f"{energia:.2e} Julios")
-# Resultado: 3.99e+24 Julios (Corregido con precisión astronómica)
+# Resultado: 3.99e+24 Julios (Corregido con precisión astronómica
