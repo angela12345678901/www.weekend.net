31fcf1bada594b870b479b6b901c5d0f39d7ba80
angela12345678901 <manyicraft@gmail.com>
2026-06-16 18:39:16 -0500

    See here for more information on the launch of Scratch 3.0: https://scratch.mit.edu/discuss/topic/326861/  Music: "Celebration" by Kool & The Gang Font: Adam.CG Pro

--- Generator.js
+++ Generator.js
@@ -9,3 +9,14 @@ const iterador = miGenerador();
 console.log(iterador.next()); // { value: "Primer valor", done: false }
 console.log(iterador.next()); // { value: "Segundo valor", done: false }
 console.log(iterador.next()); // { value: "Fin de la función", done: true }
+function* generadorId() {
+    let id = 1;
+    while (true) {
+        yield id++;
+    }
+}
+
+const contador = generadorId();
+console.log(contador.next().value); // 1
+console.log(contador.next().value); // 2
+console.log(contador.next().value); // 3
